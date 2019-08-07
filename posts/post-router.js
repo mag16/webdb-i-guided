5a8e9dcbd63fb2374ff28773be8a5c59745a5d0c
mag16 <marcoguzman16@gmail.com>
2019-08-07 11:59:59 -0700

    lecture 8/6/19

--- posts/post-router.js
+++ posts/post-router.js
@@ -5,23 +5,81 @@ const db = require('../data/db-config.js');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+
+    try {
+        const posts = await db('posts');
+        //const posts = await db.select('*').from('posts')
+        res.status(200).json(posts);
+    }
+    catch (err) {
+        res.status(500).json({message: 'Theres an error at the moment', error:err})
+    }
 
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
+    const { id } = req.params.id
+    try {
+        const [post] = await db('posts').where({ id });
+        if (post) {
+            res.status(200).json(post);
+        } else {
+            res.status(404).json({ message: `could not find post with id ${id}` });
+        }
+
+    } catch (err) {
+        res.status(500).jsonm({message:'failed to get post', error:err})
+
+    }
 
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
+    const postData = req.body;
+
+    try {
+        const post = await db('posts').insert(postData);
+        res.status(201).json(post);
+
+    } catch (err) {
+        res.status(500).json({message: 'cant add post'})
+        
+    }
 
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
+    const { id } = req.params;
+    const changes = req.body;
+
+    try { 
+        const count = await db('posts').where('id', '=', id).update(changes);
+        if (count) {
+            res.status(200).json({ updated: count });
+        } else {
+            res.status(404).json({ message: `could not find post #${id}`});
+        }
+    } catch (err) {
+        res.status(500).json({ message: 'cant add post', eroor:err })
+    }
+
 
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const count = await db('posts').where({ id }).del();
+        if(count) {
+            res.status(200).json({ deleted: count });
+        } else {
+            res.status(404).json({ message: `could not find post #${id}` });
+        }
+    } catch (err) {
+        res.status(500).json({ message: 'cant delete post', error: err })
+    }
 
 });
 
